5eed5ecc517bd35fa3c32beab52aa0498cc2eb78
lafBot <64456835+lafBot@users.noreply.github.com>
2021-03-25 16:15:20 -0600

    Updated admin page with styling

--- frontend/src/Components/Admin.js
+++ frontend/src/Components/Admin.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./Admin.css";
 import { useHistory } from "react-router-dom";
+import YodlrApi from "../API/api";
 
-const Admin = ({ users }) => {
+const Admin = ({ users, setUsers }) => {
     const history = useHistory();
 
+    async function deleteUser(e) {
+        try {
+            const id = e.target.id
+            await YodlrApi.deleteUser(id);
+            const updatedUsers = await YodlrApi.getUsers();
+            setUsers(updatedUsers);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
-        <div className="Admin my-5 mx-0">
+        <div className="Admin my-3 mx-0">
             <h1 className="my-5">Yodlr Admin Page</h1>
 
             {users.length === 0
@@ -23,11 +35,12 @@ const Admin = ({ users }) => {
                         </thead>
                         <tbody>
                             {users.map(user => (
-                                <tr className="Admin-userRow" onClick={() => history.push(`/user/${user.id}`)} >
-                                    <td>{user.firstName}</td>
-                                    <td>{user.lastName}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.state}</td>
+                                <tr className="Admin-userRow" >
+                                    <td onClick={() => history.push(`/user/${user.id}`)}>{user.firstName}</td>
+                                    <td onClick={() => history.push(`/user/${user.id}`)}>{user.lastName}</td>
+                                    <td onClick={() => history.push(`/user/${user.id}`)}>{user.email}</td>
+                                    <td onClick={() => history.push(`/user/${user.id}`)}>{user.state}</td>
+                                    <td><i id={user.id} onClick={deleteUser} class="fas fa-trash-alt"></i></td>
                                 </tr>
                             ))}
                         </tbody>
